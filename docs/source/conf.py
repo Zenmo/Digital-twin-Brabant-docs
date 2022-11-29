9bd4a87589f60f1d2d46220eafd476fdfb9351f0
Naud Loomans <38351577+naudloomans@users.noreply.github.com>
2022-11-29 15:45:25 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Brabant digital twin energy system'
+copyright = '2022, Naud Loomans'
+author = 'Naud Loomans'
 
 release = '0.1'
 version = '0.1.0'
